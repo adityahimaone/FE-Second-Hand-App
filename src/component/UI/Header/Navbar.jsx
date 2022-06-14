95cb5d6891d70b1b4dd1f3f2f902d85f02834b76
Aditya Himawan <30428194+adityahimaone@users.noreply.github.com>
2022-06-14 20:54:43 +0700

    Add Navbar & Style

--- src/component/UI/Header/Navbar.jsx
+++ src/component/UI/Header/Navbar.jsx
@@ -1,18 +1,30 @@
 import React from "react";
 import { Button } from "react-bootstrap";
+import { Form } from "react-bootstrap";
 
 function Navbar() {
   return (
-    <nav className="shadow ">
-      <div className="container d-flex justify-content-between align-items-center p-2">
-        <div>
-          <h5>Old But New</h5>
-        </div>
-        <div>
-          <Button className="bg-primary text-white">
-            <i class="bi bi-box-arrow-in-right"></i>
-            Masuk
-          </Button>
+    <nav className="shadow">
+      <div className="container p-2">
+        <div className="row d-flex justify-content-between align-items-center">
+          <div className="col-2">
+            <h5 className="w-100">Old But New</h5>
+          </div>
+          <div className="col-4">
+            <Form.Control
+              type="text"
+              id="inputPassword5"
+              aria-describedby="search"
+              className="form-input"
+              placeholder="Cari di sini ..."
+            />
+          </div>
+          <div className="col-6 d-flex justify-content-end">
+            <Button className="button-primary-1">
+              <i className="bi bi-box-arrow-in-right"></i>
+              <span className="px-2">Masuk</span>
+            </Button>
+          </div>
         </div>
       </div>
     </nav>
