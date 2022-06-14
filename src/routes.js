8f0fa71b889957744ab34196648f20db10695d14
dimas dwi putra <dimsdputra@gmail.com>
2022-06-14 22:19:31 +0700

    menambahkan component modal

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Home from "./pages/home";
+import Modal from "./component/UI/Modal/Modal"
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LayoutDefault from "./component/layout/default";
 
@@ -9,6 +10,7 @@ function RoutesPage() {
       <Routes>
         <Route path="/" element={<LayoutDefault />}>
           <Route index element={<Home />} />
+          <Route path="modal" element={<Modal />} />
         </Route>
       </Routes>
     </BrowserRouter>
