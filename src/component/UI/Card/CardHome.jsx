2f3bb4c28a9c38fd9e225bd3efeff3dce59bfb33
Aditya Himawan <30428194+adityahimaone@users.noreply.github.com>
2022-06-16 19:45:58 +0700

    Add font style

--- src/component/UI/Card/CardHome.jsx
+++ src/component/UI/Card/CardHome.jsx
@@ -7,10 +7,10 @@ function CardHome({ title, image, price, category }) {
     <Card>
       <Card.Img variant="top" className="p-2" src="/images/dummy.png" />
       <Card.Body>
-        <Card.Title>{title}</Card.Title>
+        <Card.Title className="font-14">{title}</Card.Title>
         <Card.Text>
-          <p className="color-gray">{category}</p>
-          <p>{ConvertToIDR(price)}</p>
+          <h6 className="color-gray font-10">{category}</h6>
+          <h6 className="font-14">{ConvertToIDR(price)}</h6>
         </Card.Text>
       </Card.Body>
     </Card>
