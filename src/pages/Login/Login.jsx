5a456a15455b8c0808d3af923d28390b2d50284e
Aditya Himawan <30428194+adityahimaone@users.noreply.github.com>
2022-06-20 20:25:22 +0700

    fix ci false

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import loginimg from "../../assets/image/loginimg.png";
+// import loginimg from "../../assets/image/loginimg.png";
 // import "./Login.css";
 import { useNavigate } from "react-router-dom";
 import { Button } from "react-bootstrap";
