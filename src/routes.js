d23466b6925473d70c7e457089ba5fe96768714e
FigoFajarYulianto <71810352+FigoFajarYulianto@users.noreply.github.com>
2022-07-01 05:59:52 +0700

    infoprodctbuyer

--- src/routes.js
+++ src/routes.js
@@ -20,6 +20,7 @@ import InfoPenawaran from "./pages/Penawaran/infoPenawaran";
 import InfoProfil from "./pages/InfoProfil";
 // import SellerInfoProduk from "./pages/seller/SellerInfoProduk";
 import NotificationSeller from "./pages/NotificationSeller";
+import InfoProduct from "./pages/InfoProductBuyer/InfoProduct";
 // import ModalNotifBuyer from "./components/UI/Modal_Notif_Buyer/ModalNotifBuyer";
 
 function RoutesPage() {
@@ -39,6 +40,7 @@ function RoutesPage() {
           <Route path="/product/buy/:id" element={<BuyerHalamanProduk />} />
         </Route>
         <Route path="/notification" element={<NotificationSeller />} />
+        <Route path="/infoproduct" element={<InfoProduct />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
       </Routes>
