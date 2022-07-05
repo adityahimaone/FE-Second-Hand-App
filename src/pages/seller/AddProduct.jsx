e04c9a36c18b276aadb6be40f42d6cd5673107f8
dimas dwi putra <dimsdputra@gmail.com>
2022-07-05 21:12:37 +0700

    menambahkan formik nama, harga, kategori, deskripsi

--- src/pages/seller/AddProduct.jsx
+++ src/pages/seller/AddProduct.jsx
@@ -4,33 +4,34 @@ import { useState } from "react";
 import Style from "./sellersemuaproduk.module.css";
 import { useSelector, useDispatch } from "react-redux";
 import { AxiosWithAuth } from "../../utils/axiosWithAuth";
+import { Formik, Form } from "formik";
+import * as yup from "yup";
 
 function AddProduct() {
   const { isLoading, data: loginData } = useSelector((state) => state.login);
+  let token = loginData?.data?.token;
 
-  const [nama, setNama] = useState("");
-  const [harga, setHarga] = useState("");
-  const [deskripsi, setDeskripsi] = useState("");
+  // const [kategori, setKategori] = useState("");
+  // const [harga, setHarga] = useState("");
+  // const [deskripsi, setDeskripsi] = useState("");
   const [image, setImage] = useState("");
   const [imagePrev, setImagePrev] = useState("");
   const [myOption, setMyOption] = useState("");
 
-  let token = loginData?.data?.token;
+  let schema = yup.object().shape({
+    nama: yup.string().required("nama harus di isi"),
+    harga: yup.string().required("harga Harus di isi"),
+    deskripsi: yup.string().required("deskripsi Harus di isi"),
+    kategori: yup.string().required("pilih kategori"),
+    // image: yup.mixed().required("pillih gambar"),
+  });
 
-  const handleSubmit = () => {
-    console.log("nama: ", nama);
-    console.log("harga: ", harga);
-    console.log("deskripsi: ", deskripsi);
-    console.log("image: ", image);
+  const handleSubmit = (values) => {
+    console.log(values);
 
-    const data = new FormData();
-    data.append("nama", nama);
-    data.append("harga", harga);
-    data.append("deskripsi", deskripsi);
-    data.append("image", image);
-
+    // const data = new FormData(schema);
     AxiosWithAuth(token)
-      .post("/product/add", data, {
+      .post("/product/add-product", values, {
         headers: {
           "Content-Type": "multipart/form-data",
         },
@@ -44,9 +45,10 @@ function AddProduct() {
   };
 
   const handleImagePrev = (e) => {
-    const file = e.target.files[0];
+    const file = e.currentTarget.files[0];
     setImage(file);
     setImagePrev(URL.createObjectURL(file));
+
   };
   return (
     <div className="mt-3">
@@ -55,92 +57,129 @@ function AddProduct() {
           <img src="/images/fi_arrow-left.png" alt="" />
         </div>
         <div className={`${Style["width-right"]} w-100`}>
-          <form>
-            <div className="mb-3">
-              <label className="form-label">Nama Produk</label>
-              <input
-                type="text"
-                name="name"
-                className="form-control"
-                placeholder="Nama Produk"
-                value={nama}
-                onChange={(e) => setNama(e.target.value)}
-              />
-              <span className="font-12 text-danger py-1"></span>
-            </div>
-            <div className="mb-3">
-              <label className="form-label">Harga Produk</label>
-              <input
-                type="number"
-                name="harga"
-                className="form-control"
-                placeholder="Harga Produk"
-                value={harga}
-                onChange={(e) => setHarga(e.target.value)}
-              />
-              <span className="font-12 text-danger py-1"></span>
-            </div>
-            <div className="mb-3">
-              <label className="form-label">Kategori</label>
-              <select class="form-select" aria-label="Default select example">
-                <option selected disabled>
-                  Pilih Kategori
-                </option>
-                <option value="1">One</option>
-                <option value="2">Two</option>
-                <option value="3">Three</option>
-              </select>
-            </div>
-            <div className="mb-3">
-              <label className="form-label">Deskripsi</label>
-              <div className="form-floating">
-                <textarea
-                  className="form-control"
-                  placeholder="Leave a comment here"
-                  id="floatingTextarea"
-                  style={{ height: "5rem" }}
-                  value={deskripsi}
-                  onChange={(e) => setDeskripsi(e.target.value)}
-                />
-                <label for="floatingTextarea">Deskripsi</label>
-                <span className="font-12 text-danger py-1"></span>
-              </div>
-            </div>
-            <div className="mb-3">
-              <label className="form-label">Foto Produk</label>
-              {myOption === "show" && (
+          <Formik
+            validationSchema={schema}
+            initialValues={{
+              nama: "",
+              deskripsi: "",
+              harga: "",
+              kategori: "",
+              // image: null,
+            }}
+            onSubmit={(values) => {
+              console.log(values);
+              handleSubmit(values);
+            }}
+          >
+            {({ handleSubmit, errors, handleChange, values }) => (
+              <Form onSubmit={handleSubmit}>
                 <div className="mb-3">
-                  <img src={imagePrev} alt="" style={{ maxWidth: "340px" }} />
+                  <label className="form-label">Nama Produk</label>
+                  <input
+                    type="text"
+                    name="nama"
+                    className="form-control"
+                    placeholder="Nama Produk"
+                    onChange={handleChange}
+                    value={values.nama}
+                  />
+                  <span className="font-12 text-danger py-1">
+                    {errors.nama}
+                  </span>
                 </div>
-              )}
-              <div
-                className="card-dot d-flex justify-content-center align-items-center"
-                style={{ maxWidth: "7rem" }}
-              >
-                <input
-                  type="file"
-                  onChange={(e) => handleImagePrev(e)}
-                  className={`${Style["custom-file-input"]} ms-4 `}
-                />
-              </div>
-            </div>
-            <div className="d-flex mt-5">
-              <button
-                type="button"
-                className={`${Style["button-primary-3"]} w-50 me-2`}
-                onClick={() => setMyOption("show")}
-              >
-                Preview
-              </button>
-              <button
-                type="button"
-                className="button-primary-1 w-50 ms-2"
-                onClick={handleSubmit}
-              >
-                Submit
-              </button>
-            </div>
-          </form>
+                <div className="mb-3">
+                  <label className="form-label">Harga Produk</label>
+                  <input
+                    type="number"
+                    name="harga"
+                    className="form-control"
+                    placeholder="Harga Produk"
+                    onChange={handleChange}
+                    value={values.harga}
+                  />
+                  <span className="font-12 text-danger py-1">
+                    {errors.harga}
+                  </span>
+                </div>
+                <div className="mb-3">
+                  <label className="form-label">Kategori</label>
+                  <select
+                    class="form-select"
+                    name="kategori"
+                    placeholder="pilih kategori"
+                    onChange={handleChange}
+                    value={values.kategori}
+                  >
+                    <option value="" selected disabled>
+                      Pilih Kategori
+                    </option>
+                    <option value="Kemeja">Kemeja</option>
+                    <option value="Celana">Celana</option>
+                    <option value="Kaos">Kaos</option>
+                  </select>
+                  <span className="font-12 text-danger py-1">
+                    {errors.kategori}
+                  </span>
+                </div>
+                <div className="mb-3">
+                  <label className="form-label">Deskripsi</label>
+                  <div className="form-floating">
+                    <textarea
+                      className="form-control"
+                      placeholder="Leave a comment here"
+                      id="floatingTextarea"
+                      style={{ height: "5rem" }}
+                      name="deskripsi"
+                      onChange={handleChange}
+                      value={values.deskripsi}
+                    />
+                    <label for="floatingTextarea">Deskripsi</label>
+                    <span className="font-12 text-danger py-1">
+                      {errors.deskripsi}
+                    </span>
+                  </div>
+                </div>
+                <div className="mb-3">
+                  <label className="form-label">Foto Produk</label>
+                  {myOption === "show" && (
+                    <div className="mb-3">
+                      <img
+                        src={imagePrev}
+                        alt=""
+                        style={{ maxWidth: "340px" }}
+                      />
+                    </div>
+                  )}
+                  <div
+                    className="card-dot d-flex justify-content-center align-items-center"
+                    style={{ maxWidth: "7rem" }}
+                  >
+                    <input
+                      type="file"
+                      onChange={(e) => {handleImagePrev(e)}}
+                      className={`${Style["custom-file-input"]} ms-4 `}
+                    />
+                  </div>
+                </div>
+                <div className="d-flex mt-5">
+                  <button
+                    type="button"
+                    className={`${Style["button-primary-3"]} w-50 me-2`}
+                    onClick={() => setMyOption("show")}
+                  >
+                    Preview
+                  </button>
+                  <button
+                    type="button"
+                    className="button-primary-1 w-50 ms-2"
+                    onClick={handleSubmit}
+                  >
+                    Submit
+                  </button>
+                </div>
+              </Form>
+            )}
+          </Formik>
         </div>
       </div>
     </div>
