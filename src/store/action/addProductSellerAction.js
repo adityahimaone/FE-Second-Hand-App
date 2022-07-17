b711582dd2c4bb4b6187cf75ab8e95e2a61c576f
dimas dwi putra <dimsdputra@gmail.com>
2022-07-17 14:18:14 +0700

    add product

--- src/store/action/addProductSellerAction.js
+++ src/store/action/addProductSellerAction.js
@@ -2,20 +2,24 @@
 import { AxiosWithAuth } from "utils/axiosWithAuth";
 import { POST_PRODUCT_SELLER } from "../types";
 
-export const postProductOffer = (token, data) => (dispatch) => {
-  dispatch({ type: `${POST_PRODUCT_SELLER}_LOADING` });
-  AxiosWithAuth(token)
-    .post("/product/add-product", data)
-    .then((response) => {
-      dispatch({
-        type: `${POST_PRODUCT_SELLER}_FULFILLED`,
-        payload: response.data,
+export const postAddProductSeller =
+  (token, data, setErrorHandler) => (dispatch) => {
+    dispatch({ type: `${POST_PRODUCT_SELLER}_LOADING` });
+    AxiosWithAuth(token)
+      .post("/product/add-product", data)
+      .then((response) => {
+        dispatch({
+          type: `${POST_PRODUCT_SELLER}_FULFILLED`,
+          payload: response.status,
+        });
+      })
+      .catch((error) => {
+        if (error.response.status === 400) {
+          setErrorHandler(true)
+          dispatch({
+            type: `${POST_PRODUCT_SELLER}_ERROR`,
+            error: error.response,
+          });
+        }
       });
-    })
-    .catch((error) => {
-      dispatch({
-        type: `${POST_PRODUCT_SELLER}_ERROR`,
-        error: error.message,
-      });
-    });
-};
+  };
